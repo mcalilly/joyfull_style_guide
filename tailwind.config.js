3d8cf660115eef3e7f73000e8b61cac1b1c9f418
Lee McAlilly <leemcalilly@gmail.com>
2020-05-04 16:16:33 -0500

    Add tailwind css and start custom color palette

---
+++ tailwind.config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: [
+        'Montserrat',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      display: [
+        'Montserrat',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      body: [
+      'Raleway',
+      'system-ui',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      '"Noto Sans"',
+      'sans-serif'],
+      serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif']
+    },
+
+    extend: {
+      colors: {
+        neutral: {
+          700: '#43484C',
+          900: '#2E3337'
+        },
+        primary: {
+          500: '#DE9F89',
+          800: '#D78F6B'
+        },
+      },
+    }
+  },
+  variants: {
+  }
+}
